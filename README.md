4b02eb6ed7c04c36c2dfd6f51cffb1743c56e223
Leleat <atule@pm.me>
2020-05-01 15:35:11 +0200

    update readme, added icon, minor code change

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# AGodotRememberFileDisplayMode
-Directories remember their display mode (list or thumbnail) in the split view of the FileSystem dock.
+# A Godot Plugin RememberFileDisplayMode
+
+Directories remember their display mode (list or thumbnail) in the split view of the FileSystem dock. The default is the list view.
+
+**Installation:**
+
+Either download it from the official Godot AssetLib (within Godot itself) or download the addons folder from here and move it to the root (res://) of your project. Enable the plugin in the project settings.
+
+**Preview**
+
+![gif](preview.gif)
+
--- addons/RememberFileDisplayMode/plugin.gd
+++ addons/RememberFileDisplayMode/plugin.gd
@@ -2,20 +2,19 @@ tool
 extends EditorPlugin
 
 
-const UTIL = preload("res://addons/betterFileSystemDock/util.gd")
+const UTIL = preload("res://addons/RememberFileDisplayMode/util.gd")
 var BASE_CONTROL_VBOX : VBoxContainer
 var filesystem_vsplit : VSplitContainer
 var filesystem_tree : Tree
 var filesystem_vbox : VBoxContainer
 var settings : ConfigFile
-var once : bool = true
 
 
 func _enter_tree() -> void:
 	settings = ConfigFile.new()
-	var error = settings.load("user://betterFileSystemDocksettings.cfg")
+	var error = settings.load("user://RememberFileDisplayMode.cfg")
 	if error == ERR_DOES_NOT_EXIST:
-		settings.save("user://betterFileSystemDocksettings.cfg")
+		settings.save("user://RememberFileDisplayMode.cfg")
 
 
 func _ready() -> void:
@@ -24,14 +23,13 @@ func _ready() -> void:
 	filesystem_tree = filesystem_vsplit.get_child(0)
 	filesystem_vbox = filesystem_vsplit.get_child(1)
 	filesystem_tree.connect("multi_selected", self, "_on_tree_selected")
-	filesystem_vbox.get_child(0).get_child(1).connect("pressed", self, "_on_button_file_list_display_mode_pressed")
+	
+	yield(get_tree().create_timer(.01), "timeout")
 	var split_view : bool = settings.get_value("bf", "res://", false) as bool
-	if once:
-		if (split_view and filesystem_vbox.get_child(0).get_child(1).icon == BASE_CONTROL_VBOX.get_icon("FileThumbnail", "EditorIcons")) \
-				or (not split_view and filesystem_vbox.get_child(0).get_child(1).icon == BASE_CONTROL_VBOX.get_icon("FileList", "EditorIcons")):
-			yield(get_tree().create_timer(.01), "timeout")
-			filesystem_vbox.get_child(0).get_child(1).emit_signal("pressed")
-			once = false
+	if (split_view and filesystem_vbox.get_child(0).get_child(1).icon == BASE_CONTROL_VBOX.get_icon("FileThumbnail", "EditorIcons")) \
+			or (not split_view and filesystem_vbox.get_child(0).get_child(1).icon == BASE_CONTROL_VBOX.get_icon("FileList", "EditorIcons")):
+		filesystem_vbox.get_child(0).get_child(1).emit_signal("pressed")
+	filesystem_vbox.get_child(0).get_child(1).connect("pressed", self, "_on_button_file_list_display_mode_pressed")
 
 
 func _on_tree_selected(item : TreeItem, column : int, selected : bool) -> void:
@@ -54,4 +52,4 @@ func _on_button_file_list_display_mode_pressed() -> void:
 		dir_path = selected.get_parent().get_text(0) + ("/" if selected.get_parent().get_text(0) != "res://" else "") + dir_path
 		selected = selected.get_parent()
 	settings.set_value("bf", dir_path, "" if filesystem_vbox.get_child(0).get_child(1).icon == BASE_CONTROL_VBOX.get_icon("FileThumbnail", "EditorIcons") else "true")
-	settings.save("user://betterFileSystemDocksettings.cfg")
+	settings.save("user://RememberFileDisplayMode.cfg")
